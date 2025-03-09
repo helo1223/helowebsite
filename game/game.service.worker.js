e302dc0905fdf80d116b1ad7b11533db4d3c18b7
Mark Hajnal <hajnalmarkhelo@gmail.com>
2025-03-09 02:21:23 +0100

    game stretch fix

--- game/game.service.worker.js
+++ game/game.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741482514|154853072';
+const CACHE_VERSION = '1741483260|900716311';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
